181cb88a0493c432dad59f0d813db9f142622733
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-03 15:01:31 +0530

    update 3

--- Server/server.js
+++ Server/server.js
@@ -44,7 +44,9 @@ app.use(express.json());
 
 app.set("view engine","ejs")
 app.set("views","views")
-app.get('express works')
+app.get(("/"),(req,res)=>{
+    res.send('express works')
+})
 app.get("/login",(req,res)=>{
     res.render("login")
 })
