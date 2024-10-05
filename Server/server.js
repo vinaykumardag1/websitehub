fc968b19fd311d27a820593b57e9f389a0bd1090
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-05 11:43:20 +0530

    mongodb errors

--- Server/server.js
+++ Server/server.js
@@ -1,69 +1,53 @@
-require("dotenv").config()
-const express = require("express");
-const mongoose = require("mongoose");
-const ejs = require("ejs");
-const cors = require("cors");
+
+const express=require("express")
+const mongoose=require("mongoose")
+const ejs=require("ejs")
+const cors=require("cors")
+
+const auth=require("./controllers/auth")
+const admin=require("./controllers/admin")
+const apis=require("./controllers/api")
+const app=express()
+const bodyParser=require("body-parser")
+const PORT=process.env.PORT || 4000
+const session=require("express-session")
+const cookie=require("cookie-parser")
+
+app.use(cookie())
+app.use(cors())
+app.use(session({
+    secret:process.env.SESSIONS_KEY,
+    resave:false,
+    saveUninitialized:true,
+}))
 
 
 
-const auth = require("./controllers/auth");
-const admin = require("./controllers/admin");
-const apis = require("./controllers/api");
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`)
+        .then(()=>console.log("mongodb connected"))
+        .catch(err=>console.log("error in connecting mongodb",err))
 
-const app = express();
-const PORT = process.env.PORT || 4000;
-
-
-
-app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.set("view engine","ejs")
+app.set("views","views")
+app.get(("/"),(req,res)=>{
+    res.send('express works')
+})
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+app.post('/admin',admin.admin);
+app.get("/api",apis.api)
+app.get("/api/:category",apis.api_category);
+app.post("/login",auth.login)
 
 
-const connectWithRetry = () => {
-  console.log("Attempting MongoDB connection...");
-  mongoose
-    .connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.mongodb.net/webhub?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
 
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000, // Timeout of 30 seconds
-      }
-     
-    )
-    .then(() => {
-      console.log("MongoDB connected successfully");
-    })
-    .catch((err) => {
-      console.error("Error in connecting MongoDB, retrying in 5 seconds...", err);
-   
-    });
-};
-
-connectWithRetry();
-
-
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-// Routes
-app.get("/", (req, res) => {
-    res.send("Express works");
-});
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-app.post("/admin", admin.admin);
-app.get("/api", apis.api);
-app.get("/api/:category", apis.api_category);
-app.post("/login", auth.login);
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
