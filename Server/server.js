9f781640edbf27e6c725ffd9ed80f53d30ae933c
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-05 12:36:49 +0530

    errors solved

--- Server/server.js
+++ Server/server.js
@@ -16,23 +16,11 @@ app.use(cors())
 
 
 
-// mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`)
-//         .then(()=>console.log("mongodb connected"))
-//         .catch(err=>console.log("error in connecting mongodb",err))
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@db:27017/webhub?retryWrites=true&w=majority`)
+        .then(()=>console.log("mongodb connected"))
+        .catch(err=>console.log("error in connecting mongodb",err))
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
