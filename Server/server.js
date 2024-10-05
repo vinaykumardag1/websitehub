d6457e06f41036e542bd713d089af8064a0d8af2
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-05 12:14:53 +0530

    mongodb api

--- Server/server.js
+++ Server/server.js
@@ -3,30 +3,36 @@ const express=require("express")
 const mongoose=require("mongoose")
 const ejs=require("ejs")
 const cors=require("cors")
-
 const auth=require("./controllers/auth")
 const admin=require("./controllers/admin")
 const apis=require("./controllers/api")
 const app=express()
 const bodyParser=require("body-parser")
 const PORT=process.env.PORT || 4000
-const session=require("express-session")
-const cookie=require("cookie-parser")
 
-app.use(cookie())
+
+
 app.use(cors())
-app.use(session({
-    secret:process.env.SESSIONS_KEY,
-    resave:false,
-    saveUninitialized:true,
-}))
 
 
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`)
-        .then(()=>console.log("mongodb connected"))
-        .catch(err=>console.log("error in connecting mongodb",err))
+// mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`)
+//         .then(()=>console.log("mongodb connected"))
+//         .catch(err=>console.log("error in connecting mongodb",err))
 
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
