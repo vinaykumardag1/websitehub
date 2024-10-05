a68b11f1f305c22b8b35ba502ca8831cfbc1bed6
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-05 10:52:21 +0530

    solving erros

--- Server/controllers/api.js
+++ Server/controllers/api.js
@@ -1,10 +1,20 @@
-const User=require("../models/User")
-module.exports.api=async(req,res)=>{
-    const getData=await User.find({})
-    res.send(getData)
-}
-module.exports.api_category=async (req,res)=>{
-    const category = req.params.category;
-    const getData = await User.find({ category: category });
-    res.send(getData);
-}
+const User = require("../models/User");
+
+module.exports.api = async (req, res) => {
+    try {
+        const getData = await User.find({});
+        res.status(200).send(getData);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching data", error });
+    }
+};
+
+module.exports.api_category = async (req, res) => {
+    try {
+        const category = req.params.category;
+        const getData = await User.find({ category });
+        res.status(200).send(getData);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching data by category", error });
+    }
+};
--- Server/server.js
+++ Server/server.js
@@ -1,62 +1,67 @@
+// Ensure you have .env configured
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const cors = require("cors");
+const session = require("express-session");
+const cookie = require("cookie-parser");
 
-const express=require("express")
-const mongoose=require("mongoose")
-const ejs=require("ejs")
-const cors=require("cors")
+// Importing routes and controllers
+const auth = require("./controllers/auth");
+const admin = require("./controllers/admin");
+const apis = require("./controllers/api");
 
-const auth=require("./controllers/auth")
-const admin=require("./controllers/admin")
-const apis=require("./controllers/api")
-const app=express()
-const bodyParser=require("body-parser")
-const PORT=process.env.PORT || 4000
-const session=require("express-session")
-const cookie=require("cookie-parser")
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use(cookie())
-app.use(cors())
-app.use(session({
-    secret:process.env.SESSIONS_KEY,
-    resave:false,
-    saveUninitialized:true,
-}))
-
-
-
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`)
-        .then(()=>console.log("mongodb connected"))
-        .catch(err=>console.log("error in connecting mongodb",err))
-
-app.use(bodyParser.urlencoded({extended:true}))
+// Middlewares
+app.use(cookie());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session({
+    secret: process.env.SESSIONS_KEY,
+    resave: false,
+    saveUninitialized: true,
+}));
 
-app.set("view engine","ejs")
-app.set("views","views")
-app.get(("/"),(req,res)=>{
-    res.send('express works')
+// MongoDB connection
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-app.get("/login",(req,res)=>{
-    res.render("login")
-})
-app.get("/admin",auth.noCache,auth.getAdmin)
-app.post('/admin',admin.admin);
-app.get("/api",apis.api)
-app.get("/api/:category",apis.api_category);
-app.post("/",auth.login)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("Error in connecting MongoDB", err));
 
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-process.on('unhandledRejection', (err) => {
-    console.error('Unhandled rejection:', err);
+// Routes
+app.get("/", (req, res) => {
+    res.send("Express works");
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.get("/admin", auth.noCache, auth.getAdmin);
+app.post("/admin", admin.admin);
+app.get("/api", apis.api);
+app.get("/api/:category", apis.api_category);
+app.post("/login", auth.login);
+
+// Error handling for unhandled promises and exceptions
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled rejection:", err);
+    process.exit(1);
+});
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught exception:", err);
     process.exit(1);
 });
 
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught exception:', err);
-    process.exit(1);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
-})
