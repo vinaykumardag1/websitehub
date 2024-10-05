8cba5498742e4b73e96136adeeb3b1f25878032b
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-05 11:03:00 +0530

    solving errors

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,4 +1,3 @@
-require("dotenv").config()
 
 module.exports.login=(req,res)=>{
     const username=req.body.username
@@ -11,19 +10,4 @@ module.exports.login=(req,res)=>{
     }
     
 }
-module.exports.noCache=(req, res, next)=> {
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-    res.set('Pragma', 'no-cache');
-    res.set('Expires', '0');
-    next()
-  }
-  module.exports.getAdmin=async(req,res)=>{
-    req.session.username=process.env.MONGODB_USERNAME
-    res.cookie("sessionId",req.sessionID)
-    if (req.session.username) {
-        res.render('admin');
-    } else {
-        res.redirect('/');
-    }
-  }
-  
+
--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,11 @@
-// Ensure you have .env configured
+
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const cors = require("cors");
-const session = require("express-session");
-const cookie = require("cookie-parser");
 
-// Importing routes and controllers
+
+
 const auth = require("./controllers/auth");
 const admin = require("./controllers/admin");
 const apis = require("./controllers/api");
@@ -14,19 +13,15 @@ const apis = require("./controllers/api");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middlewares
-app.use(cookie());
+
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(session({
-    secret: process.env.SESSIONS_KEY,
-    resave: false,
-    saveUninitialized: true,
-}));
 
-// MongoDB connection
+
+
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@atlascluster.8tdja.mongodb.net/webhub?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -34,7 +29,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MO
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log("Error in connecting MongoDB", err));
 
-// Set view engine
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -51,7 +46,6 @@ app.get("/api", apis.api);
 app.get("/api/:category", apis.api_category);
 app.post("/login", auth.login);
 
-// Error handling for unhandled promises and exceptions
 process.on("unhandledRejection", (err) => {
     console.error("Unhandled rejection:", err);
     process.exit(1);
@@ -61,7 +55,6 @@ process.on("uncaughtException", (err) => {
     process.exit(1);
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
