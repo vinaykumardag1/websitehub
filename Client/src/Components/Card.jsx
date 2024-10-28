8d633d4b1b8101b49b3976e8890465ecb0d3c226
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-10-28 11:26:22 +0530

    anchor tag issue

--- Client/src/Components/Card.jsx
+++ Client/src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ item,index }) => {
     <div>
       <ul className='border-4 ease-linear duration-200 hover:scale-[110%] p-5 border-sky-100 rounded-2xl' key={index}>
         <li className='text-3xl py-3'>{item.name}</li>
-        <a href={item.url} target='_blank' rel="noopener noreferrer">
+        <a href={item.url} >
           <li className='py-3 text-blue-400'>{item.name}.com</li>
         </a>
         <li className='py-3'>{item.desc}</li>
