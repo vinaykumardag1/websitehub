6574529742dfe39a653980210f24aac1b0bb6ae7
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2024-11-27 17:20:02 +0530

    ulr update

--- Client/src/Components/Card.jsx
+++ Client/src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ item,index }) => {
     <div>
       <ul className='border-4 ease-linear duration-200 hover:scale-[110%] p-5 border-sky-100 rounded-2xl' key={index}>
         <li className='text-3xl py-3' key={index}>{item.name}</li>
-        <a href={item.url}  >
+        <a href={`${item.url}`}  >
           <li className='py-3 text-blue-400' title={item.url}>{item.name}</li>
         </a>
         <li className='py-3'>{item.desc}</li>
