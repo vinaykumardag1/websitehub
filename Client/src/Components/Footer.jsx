86740f388a080649b712aa4bfe06982970870fde
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2025-01-11 11:54:04 +0530

    gooter ttl3 ad dto links

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -10,11 +10,11 @@ const Footer = () => {
     <Container>
     <div className=' py-10'>
       <ul className='flex w-full justify-center text-3xl items-center gap-9' type='none'>
-        <li><a href="mailto:vinaykumardag1@gmail.com"><CiMail alt="mail icon"/></a></li>
-        <li><a href="https://www.instagram.com/daggupati_vinay/" target='_blank'><FaInstagram alt="mail icon"/></a></li>
-        <li><a href="https://x.com/vinay_daggupati"><FaXTwitter/></a></li>
-        <li><a href="https://www.linkedin.com/in/vinay-kumar-daggupati-b3141224b/"><CiLinkedin/></a></li>
-        <li><a href="https://github.com/vinaykumardag1"><FaGithub/></a></li>
+        <li><a href="mailto:vinaykumardag1@gmail.com" title="mail "><CiMail alt="mail icon" /></a></li>
+        <li><a href="https://www.instagram.com/daggupati_vinay/" title="Instagram " target='_blank'><FaInstagram alt="mail icon"/></a></li>
+        <li><a href="https://x.com/vinay_daggupati" title="Twitter"><FaXTwitter/></a></li>
+        <li><a href="https://www.linkedin.com/in/vinay-kumar-daggupati-b3141224b/" title="Linked In"><CiLinkedin/></a></li>
+        <li><a href="https://github.com/vinaykumardag1" title="github "><FaGithub/></a></li>
       </ul>
      </div>
      <p className='text-center my-4'>© 2024 Daggupati Vinay. All rights reserved.</p>
