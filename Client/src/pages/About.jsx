88693658bd8653eff36f837e396b1f3aba85923c
vinaykumardag1 <112471376+vinaykumardag1@users.noreply.github.com>
2025-02-21 10:14:25 +0530

    update

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -9,11 +9,11 @@ const About = () => {
       <Navbar/>
       <Container>
         <div className='px-6 flex  gap-5 flex-col-reverse  md:flex-row py-10'>
-          <div className='text-sm text-white text-justify md:text-xl flex items-center '>
-            <div>
+          <div className='px-20 text-sm text-white text-justify md:text-xl flex items-center '>
+           
             <p>WebsiteHub is designed to be your go-to resource for discovering the internet's top websites across various categories. Our platform organizes the most popular and trusted websites from sectors like news, e-commerce, entertainment, social media, and technology, giving users easy access to high-quality online destinations. Each website listed is curated based on performance metrics like traffic, engagement, and overall reputation, ensuring that users receive a carefully selected overview of the best the web has to offer.</p>
             <p className='py-5'>Whether you're conducting market research, curating content, or just exploring industry trends, WebsiteHub simplifies the search for reliable information and quality sites, making it an invaluable tool for anyone navigating today’s digital landscape.</p>
-            </div>
+            
           </div>
           <img src={about_img1} alt='Image' className='md:w-1/3'/>
         </div>
